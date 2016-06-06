0c8d7f5174bd245ef52c2c9e58e95e09a4dcee7a
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-06-06 16:23:40 +0200

    split particles header and source

--- include/particles.h
+++ include/particles.h
@@ -1,6 +1,4 @@
 #pragma once
-#include <vector>
-#include <complex>
 #include <assert.h>
 
 struct Particles{
@@ -10,66 +8,14 @@ struct Particles{
 
   Particles(){is_a_copy=true;}
   Particles(int N0);
-  inline Particles(double* ,double* ,double* ,const int );
+  inline Particles(double* x0,double* y0,double* w0,const int N0):
+    x(x0),y(y0),w(w0),N(N0),is_a_copy(true){}
   ~Particles();
-  inline Particles subEnsamble(int i0,int l) const;
+  inline Particles subEnsamble(int i0,int l) const{
+    assert(i0+l <= N);
+    return Particles(x+i0,y+i0,w+i0,l);
+  }
   void resize(int N);
   friend void swap(Particles& a,Particles& b);
 };
 
-Particles::Particles(int N0):N(N0)
-{
-  is_a_copy=false;
-  x = new double[N];
-  y = new double[N];
-  w = new double[N];
-}
-
-Particles::~Particles()
-{
-  if(not is_a_copy) {
-    delete[] x;
-    delete[] y;
-    delete[] w;
-  }
-}
-
-Particles::Particles(double* x0,double* y0,double* w0,const int N0):
-x(x0),y(y0),w(w0),N(N0),is_a_copy(true)
-{}
-
-Particles Particles::subEnsamble(int i0, int l) const{
-  assert(i0+l <= N);
-  return Particles(x+i0,y+i0,w+i0,l);
-}
-
-void Particles::resize(int N0) {
-  N=N0;
-  if(not is_a_copy) {
-    delete[] x;
-    delete[] y;
-    delete[] w;
-  }
-  is_a_copy=false;
-  x = new double[N];
-  y = new double[N];
-  w = new double[N];
-}
-
-void swap(Particles& a,Particles& b){
-  assert(not (a.is_a_copy and b.is_a_copy));
-  double *xtmp(a.x),*ytmp(a.y),*wtmp(a.w);
-  int ntmp=a.N;
-  a.x=b.x; a.y=b.y; a.w=b.w;
-  a.N=b.N;
-  b.x=xtmp; b.y=ytmp; b.w=wtmp;
-  b.N=ntmp;
-}
-
-#include<iostream>
-using std::cout; using std::endl;
-void Print(const Particles& p,int n=0){
-  n= n? std::min(n,p.N) : p.N;
-  cout<<"x\ty\tw\n";
-  for(int i=0;i<n;i++) cout<<p.x[i]<<"\t"<<p.y[i]<<"\t"<<p.w[i]<<endl;
-}
