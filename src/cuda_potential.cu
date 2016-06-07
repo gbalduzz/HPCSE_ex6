38316dda681d79a2cdcd4c97e8d267a8b1bb15eb
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-06-07 17:02:41 +0200

    added one timer

--- src/cuda_potential.cu
+++ src/cuda_potential.cu
@@ -1,14 +1,18 @@
 #include <cmath>
 #include "cuda_potential.hpp"
 #include "cuda_vector.hpp"
+#include "timing.h"
 #include "cuda_kernels.cuh"
 #include "p2e.h"
+#include<iostream>
+using std::coput; using std::endl;
 constexpr int threadsPerBlock = 128;
 
 
 
 
 void cudaPotential(const Particles& p, Particles& t, const int order){
+  reset_and_start_timer();
   const int Np= p.N;
   const int Nt= t.N;
   // start copying while compuing p2e
@@ -17,9 +21,10 @@ void cudaPotential(const Particles& p, Particles& t, const int order){
   std::vector<double> crd(order+1,0),cid(order+1,0);
   p2e(p, crd,cid);
   CudaVector<double> cr(order+1,crd.data()),ci(order+1,cid.data());
-//todo time
+//todo e2p time
   e2p<<<Np/threadsPerBlock,threadsPerBlock>>>(t.x,t.y,t.w,cr,ci,Nt);
-
   dtw.copyTo(t.w);
+  double tf = get_elapsed_mcycles();
+  cout<<"CUDA total M cycles (CPU p2e + GPU e2p) : "<<t1<<endl;
 }
  
