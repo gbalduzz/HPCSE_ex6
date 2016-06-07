b22d4e0f357649b2c047c778c73fd2705a532611
gbalduzz <giobaldu@gmial.com>
2016-06-07 17:08:46 +0200

    fixup

--- src/cuda_potential.cu
+++ src/cuda_potential.cu
@@ -4,8 +4,8 @@
 #include "timing.h"
 #include "cuda_kernels.cuh"
 #include "p2e.h"
-#include<iostream>
-using std::coput; using std::endl;
+#include <iostream>
+using std::cout; using std::endl;
 constexpr int threadsPerBlock = 128;
 
 
@@ -25,6 +25,6 @@ void cudaPotential(const Particles& p, Particles& t, const int order){
   e2p<<<Np/threadsPerBlock,threadsPerBlock>>>(t.x,t.y,t.w,cr,ci,Nt);
   dtw.copyTo(t.w);
   double tf = get_elapsed_mcycles();
-  cout<<"CUDA total M cycles (CPU p2e + GPU e2p) : "<<t1<<endl;
+  cout<<"CUDA total M cycles (CPU p2e + GPU e2p) : "<<tf<<endl;
 }
  
