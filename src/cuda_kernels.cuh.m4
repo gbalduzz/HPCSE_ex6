2c3d2e39e9dfac5cadc9d25fafc037676fc92ed8
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-06-07 10:56:52 +0200

    generate cuda kernels with m4. To check

---
+++ src/cuda_kernels.cuh.m4
@@ -0,0 +1,45 @@
+include(src/unroll.m4)
+
+__global__ void p2e(double* xv,double* yv, double* wv,double* cr,double* ci, const int N){
+  //cr and ci must have order+1 reserved spaces
+  const int i=blockIdx.x*blockDim.x+threadIdx.x;
+  if(i<N){
+      const double w=wv[i];
+      cr[0]+=w;
+      const double zr_1=xv[i];
+      const double zi_1=yv[i];
+      cr[1] -= w*zr_1;
+      ci[1] -= w*zi_1;
+      LUNROLL(k,2,eval(ORDER),`
+      //loop iteration
+      const double TMP(zr,k) = TMP(zr,eval(k-1))*zr_1 - TMP(zi,eval(k-1))*zi_1;
+      const double TMP(zi,k) = TMP(zr,eval(k-1))*zi_1 + TMP(zi,eval(k-1))*zr_1;
+      cr[k] -= w*TMP(zr,k);
+      ci[k] -= w*TMP(zi,k);
+      ')
+  }
+}
+
+__global__  void e2p(const double* xv,const double* yv, double* rv,
+                const double* cr,const double* ci, const int N){
+  const int i=blockIdx.x*blockDim.x+threadIdx.x;
+  if(i<N) {
+  
+   const double z_re = xv[i];
+   const double z_im = yv[i];
+   const  double r2 = z_re*z_re+z_im*z_im;
+   //cr[0]=Q
+   double result = cr[0]*0.5*log(r2);
+   const  double pow_re_1 = z_re / r2;
+   const  double pow_im_1 = -z_im/r2;
+   result += c_re[1]*pow_re_1-c_im[1]*pow_im_1;
+   
+   LUNROLL(k,2,eval(ORDER),`
+  // loop iteration
+    const double TMP(pow_re,k) = TMP(pow_re,eval(k-1))*pow_re_1 - TMP(pow_im, eval(k-1))*pow_im_1;
+    const double TMP(pow_im,k) = TMP(pow_im, eval(k-1))*pow_re_1 + TMP(pow_re, eval(k-1))*pow_im_1;
+    result += c_re[k]*TMP(pow_re,k)-c_im[k]*TMP(pow_im,k);
+  ')
+  rv[i] = result; 
+  }
+}
--- src/cuda_potential.cu
+++ src/cuda_potential.cu
@@ -1,29 +1,9 @@
 #include <cmath>
 #include "cuda_potential.hpp"
 #include "cuda_vector.hpp"
+#include "cuda_kernels.cuh"
 constexpr int threadsPerBlock = 128;
 
-__global__ void p2e(double* xv,double* yv, double* wv,double* cr,double* ci, const int N,const int order){
-  //cr and ci must have order+1 reserved spaces
-  const int i=blockIdx.x*blockDim.x+threadIdx.x;
-  if(i<N){
-      const double x=xv[i];
-      const double y=yv[i];
-      const double w=wv[i];
-      cr[0]+=w;
-      double z_re = x;
-      double z_im = y;
-
-    for(int k=1;k<=order;k++){
-      cr[k]-=w*z_re;
-      ci[k]-=w*z_im;
-      //compute z=(x+i y)^k
-      const double temp = z_re*x-z_im*y;
-      z_im= z_re*y+z_im*x;
-      z_re = temp;
-    }
-  }
-}
 
 __global__ void divide(double* cr,double* ci, const int order){
   //call with one block
@@ -31,28 +11,6 @@ __global__ void divide(double* cr,double* ci, const int order){
   if(i>1 && i<=order){
     ci[i] /= i;
     cr[i] /= i;
-  }
-}
-
-__global__  void e2p(const double* xv,const double* yv, double* rv,
-                const double* cr,const double* ci, const int N, const int order){
-  const int i=blockIdx.x*blockDim.x+threadIdx.x;
-  if(i<N) {
-    //cr[0]=Q
-    const double x = xv[i];		   
-    const double y = yv[i];
-    double result = cr[0] * 0.5 * std::log(x * x + y * y);
-    double zr = 1, zi = 0;
-   
-//#pragma unroll(4)
-    for (int k = 1; k < order + 1; k++) {
-      const double temp = zr * x - zi * y;
-      zi = zr * y + zi * x;
-      zr = temp;
-      //result += real(a[k]/z**k)
-      result += (cr[k] * zr + ci[k] * zi) / (zr * zr + zi * zi);
-    }
-    rv[i] = result;
   }
 }
 
@@ -66,9 +24,9 @@ void cudaPotential(const Particles& p, Particles& t, const int order){
   // are cr,ci set to zero?
   CudaVector<double> cr(order+1),ci(order+1);
 
-  p2e<<<Np/threadsPerBlock,threadsPerBlock>>>(p.x,p.w,p.w,cr,ci,Np,order);
+  p2e<<<Np/threadsPerBlock,threadsPerBlock>>>(p.x,p.w,p.w,cr,ci,Np);
   divide<<<1,order+1>>>(cr,ci,order);
-  e2p<<<Np/threadsPerBlock,threadsPerBlock>>>(t.x,t.y,t.w,cr,ci,Nt,order);
+  e2p<<<Np/threadsPerBlock,threadsPerBlock>>>(t.x,t.y,t.w,cr,ci,Nt);
 
   dtw.copyTo(t.w);
 }
