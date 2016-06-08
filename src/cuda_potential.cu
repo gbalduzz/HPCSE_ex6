1bd00adc2bc3d8733dac601eb11d480edb200c90
gbalduzz <giobaldu@gmial.com>
2016-06-08 14:10:40 +0200

    time GPU execution. initialize  result to zero as precaution. Bug discovered

--- src/cuda_potential.cu
+++ src/cuda_potential.cu
@@ -12,19 +12,32 @@ constexpr int threadsPerBlock = 128;
 
 
 void cudaPotential(const Particles& p, Particles& t, const int order){
-  reset_and_start_timer();
   const int Np= p.N;
   const int Nt= t.N;
+  //assure no influence forom previous computation
+  std::fill_N(t.w,Nt,0);
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);  
+ 
+  reset_and_start_timer();
   // start copying while compuing p2e
   CudaVector<double> dtx(Nt,t.x),dty(Nt,t.y);
   CudaVector<double> dtw(Nt);
   std::vector<double> crd(order+1,0),cid(order+1,0);
   p2e(p, crd,cid);
   CudaVector<double> cr(order+1,crd.data()),ci(order+1,cid.data());
-//todo e2p time
+//time e2p
+  cudaEventRecord(start);
   e2p<<<Np/threadsPerBlock,threadsPerBlock>>>(t.x,t.y,t.w,cr,ci,Nt);
+  cudaEventRecord(stop);
   dtw.copyTo(t.w);
+  cudaEventSynchronize(stop);
   double tf = get_elapsed_mcycles();
+
+
   cout<<"CUDA total M cycles (CPU p2e + GPU e2p) : "<<tf<<endl;
+  float GPU_ms; cudaEventElapsedTime(&GPU_ms, start, stop );
+  cout<<"Time for e2p kernel: "<<GPU_ms<<endl;
 }
  
